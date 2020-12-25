b7c2c06805fac7d91a0f384a7d118ec631221977
jen <leungjen18@gmail.com>
2020-12-25 21:42:42 +0800

    add guess times

--- guess.py
+++ guess.py
@@ -1,22 +1,22 @@
 import random
 r = random.randint(1, 100)
-x = 3
+x = 0
 
 while True:
+	x = x + 1
 	guess = input("來猜猜看: ")
 	guess = int(guess)
+	
 	if guess == r :
 		print ("u get the correct ans!")
 		break
 	else:
-		x = x - 1
 		if guess > r :
 			print("你的猜測比答案大喔")
 		elif guess < r :
 			print("你的猜測比答案小喔")
-		if x == 0 :
-			print ("答案是", r, "沒有機會了喔")
-			break
+		print ("你猜了", x, "次")
+
 
 
 
